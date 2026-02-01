03dac9945dac77a7bd10d0c268e0625a144a9be0
dudu <eduardo.cereza@outlook.com>
2026-02-01 16:18:04 -0300

    entidade construida

--- models/snack.py
+++ models/snack.py
@@ -1,2 +1,8 @@
-class Snack:
-    print("CLASSE CRIADA!")
+from database import db
+
+class Snack(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    hasDiet = db.Column(db.Boolean, nullable=False)
