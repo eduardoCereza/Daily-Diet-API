4c3fcad0ec83f500681de9baa1c2283694e589b3
dudu <eduardo.cereza@outlook.com>
2026-02-01 16:04:46 -0300

    iniciando db

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from models.snack import Snack
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '12345'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db' #Caminho do banco de dados
 
 if __name__ == '__main__':
     app.run(debug=True)
